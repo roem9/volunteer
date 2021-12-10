a644c960c9a0f216033a13c1feba6d46eec3f013
roem09 <muhammadrum04@gmail.com>
2021-12-10 16:56:30 +0700

    first commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -17,7 +17,7 @@ class Home extends MY_Controller {
         $data['link_wa'] = "https://wa.me/{$data['marketing']['no_wa']}?text=Halo%20{$data['marketing']['panggilan']}%20{$nama_panggilan}%2C%20mau%20tau%20info%20Kavling%20Siap%20Bangun%20Klaster%20Nayanika%20di%20Setu%20Bekasi%20dong%20{$data['marketing']['panggilan']}%3F";
 
         if($data['marketing']){
-            $this->load->view("klasternayanika/index", $data);
+            // $this->load->view("klasternayanika/index", $data);
         } else {
             redirect(base_url()."0910");
         }
