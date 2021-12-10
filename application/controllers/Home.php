0d217b699243727b6c6dcb39d22351ace2673da4
roem09 <muhammadrum04@gmail.com>
2021-12-10 19:21:58 +0700

    initial commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,17 +6,17 @@ class Home extends MY_Controller {
     
     public function id($id){
         $data['marketing'] = $this->home->get_one("marketing", ["id" => $id]);
-
-        $replace_wa = array(
-            ' ' => '%20',
-            '"' => '%22'
-        );
-
-        $nama_panggilan = str_replace(array_keys($replace_wa), $replace_wa, $data['marketing']['nama_panggilan']);
-
-        $data['link_wa'] = "https://wa.me/{$data['marketing']['no_wa']}?text=Halo%20{$data['marketing']['panggilan']}%20{$nama_panggilan}%2C%20mau%20tau%20info%20Kavling%20Siap%20Bangun%20Klaster%20Nayanika%20di%20Setu%20Bekasi%20dong%20{$data['marketing']['panggilan']}%3F";
-
         if($data['marketing']){
+
+            $replace_wa = array(
+                ' ' => '%20',
+                '"' => '%22'
+            );
+
+            $nama_panggilan = str_replace(array_keys($replace_wa), $replace_wa, $data['marketing']['nama_panggilan']);
+
+            $data['link_wa'] = "https://wa.me/{$data['marketing']['no_wa']}?text=Halo%20{$data['marketing']['panggilan']}%20{$nama_panggilan}%2C%20mau%20tau%20info%20Kavling%20Siap%20Bangun%20Klaster%20Nayanika%20di%20Setu%20Bekasi%20dong%20{$data['marketing']['panggilan']}%3F";
+
             $this->load->view("klasternayanika/index", $data);
         } else {
             redirect(base_url()."0910");
